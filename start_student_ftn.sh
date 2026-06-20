abd1ffbaca89f1f810e82f612d5e1eb6a2411f49
Nikola Mandic <mandic.nikola96@gmail.com>
2026-06-20 08:10:21 +0200

    feat: add only script for student-ftn app

---
+++ start_student_ftn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e  # prekini ako neka komanda padne
+
+# 0. Provera da li Docker postoji
+if ! command -v docker &> /dev/null; then
+    echo "❌ Docker nije instaliran ili nije u PATH. Instaliraj Docker pre pokretanja ove skripte."
+    exit 1
+else
+    echo "✅ Docker je pronađen."
+fi
+
+# Detect docker compose command
+if docker compose version &> /dev/null; then
+    COMPOSE_CMD="docker compose"
+elif docker-compose version &> /dev/null; then
+    COMPOSE_CMD="docker-compose"
+else
+    echo "❌ Nema ni docker compose ni docker-compose instaliran."
+    exit 1
+fi
+
+echo ">> Koristim: $COMPOSE_CMD"
+
+# 1. Pređi na Desktop korisnika
+cd ~/Desktop
+echo ">> Trenutni direktorijum: $(pwd)"
+
+# 2. Ako folder postoji -> obriši ga
+if [ -d "testlink-docker" ]; then
+  echo ">> Brišem postojeći folder testlink-docker..."
+  rm -rf testlink-docker
+fi
+
+# 3. Kloniranje repozitorijuma
+echo ">> Kloniram repozitorijum..."
+git clone https://github.com/MandicNikola/testlink-docker
+
+# 4. Ulazak u direktorijum
+cd testlink-docker
+
+cd student-ftn
+
+echo ">> Pokrećem docker compose down za StudentFTN sa build-om..."
+$COMPOSE_CMD down -v
+
+echo ">> Pokrećem docker compose za StudentFTN sa build-om..."
+$COMPOSE_CMD up --build -d
+
+
+
+
